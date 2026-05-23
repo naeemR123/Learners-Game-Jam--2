0d780c98ed9ac1971328c8b73d6719a265a492b9
naeemR123 <naeem4273@gmail.com>
2026-05-22 19:26:31 -0700

    Revert "Test"
    This reverts commit babeba55226f38bd4708ee3e715cd1cb722cb43d.

--- Scripts/ResourceLabel.gd
+++
@@ -1,8 +0,0 @@
-# res://scripts/ResourceLabel.gd
-extends Label
-
-func _ready():
-	GameManager.resource_changed.connect(_update_text)
-
-func _update_text(amount):
-	text = "Resources: %d" % amount
