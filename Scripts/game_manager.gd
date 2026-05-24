3da395c541883ef31c68f19880cb8c6cc117347b
naeemR123 <naeem4273@gmail.com>
2026-05-24 03:49:35 -0700

    Tweaked upgrade management with ui

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -6,17 +6,20 @@ var turret_upgrade_cost: int = 10
 var turret_upgrade_level: int = 1
 
 @export var slow_down_amount: float = 0.75
-
+var tractor_upgrade_cost: int = 10
+var tractor_upgrade_level: int = 1
 
 signal turret_upgrade(new_amount)
-signal resources_changed(new_amount)
+signal resources_changed()
 
 
 func add_resource(amount: int):
 	resources += amount
-	resources_changed.emit(resources)
+	resources_changed.emit()
 
 
 func update_firerate(amount: float):
 	turret_fire_rate -= amount
+	if turret_fire_rate < 0.05:
+		turret_fire_rate = 0.05
 	turret_upgrade.emit(turret_fire_rate)
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -4,16 +4,17 @@ extends CanvasLayer
 
 
 @onready var label: Label = $Label
-@onready var upgrade_button: Button = $TurretUpgrade
+@onready var turret_upgrade: Button = $HBoxContainer/TurretUpgrade
+@onready var slow_down_upgrade: Button = $HBoxContainer/SlowDownUpgrade
 
 
 func _ready() -> void:
 	game.resources_changed.connect(resource_label)
-	update_button(game.turret_fire_rate)
-	
+	turret_button_refresh(0.2 * game.turret_upgrade_level)
+	tractor_button_refresh()
 
-func resource_label(new_amount):
-	update_ui(new_amount)
+func resource_label():
+	update_ui()
 
 
 func _on_turret_upgrade_pressed() -> void:
@@ -26,17 +27,42 @@ func _on_turret_upgrade_pressed() -> void:
 		
 		game.turret_upgrade_level += 1
 		
-		label.text = "Resources: " + str(game.resources) + "\nLevel: " + str(game.turret_upgrade_level)
-		game.turret_upgrade_cost = game.turret_upgrade_cost * game.turret_upgrade_level
-		update_button(upgrade_amount)
+		game.turret_upgrade_cost = int(10 * pow(1.15,game.turret_upgrade_level))
+		turret_button_refresh(upgrade_amount)
 
-func update_button(speed_amount):
-	upgrade_button.text = "Upgrade Turret Speed: +" + str(speed_amount) + "\nCost: " + str(game.turret_upgrade_cost)
+func turret_button_refresh(speed_amount):
+	turret_upgrade.text = "Upgrade Turret Speed: +" + str(speed_amount) + "\nCost: " + str(game.turret_upgrade_cost)
 
 
-func update_ui(new_amount):
-	label.text = "Resources: " + str(new_amount) + "\nLevel: " + str(game.turret_upgrade_level)
-
+func update_ui():
+	var display_text = "Resource: " + str(game.resources)
+	
+	if game.turret_upgrade_level > 1:
+		display_text += "\nTurret Level: " + str(game.turret_upgrade_level)
+		
+	if game.tractor_upgrade_level > 1:
+		display_text += "\nTractor Level: " + str(game.tractor_upgrade_level)
+	
+	label.text = display_text
+	
 
 func _on_slow_down_upgrade_pressed() -> void:
-	pass
+	if game.resources >= game.tractor_upgrade_cost:
+		if game.slow_down_amount <= 0.1:
+			return
+		game.resources -= game.tractor_upgrade_cost
+		update_ui()
+		
+		game.slow_down_amount -= 0.05 * game.tractor_upgrade_level
+		
+		if game.slow_down_amount < 0.1:
+			game.slow_down_amount = 0.1
+		
+		game.tractor_upgrade_level += 1
+		game.tractor_upgrade_cost = int(10 * pow(1.15,game.tractor_upgrade_level))
+		tractor_button_refresh()
+		
+
+
+func tractor_button_refresh():
+	slow_down_upgrade.text =  "Upgrade Slow Down Speed: +" + str(0.05 * game.tractor_upgrade_level) + "\nCost: " + str(game.tractor_upgrade_cost)
